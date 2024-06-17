c301773e8c63d6a34f0df40f9406e14c20267165
Haritha Desapriya <158765525+harithaDes@users.noreply.github.com>
2024-06-17 21:16:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const display = document.getElementById("display");
+
+
+function appendToDisplay(input)
+{
+   display.value += input;
+}
+
+function calculate()
+{
+    try{
+    display.value = eval(display.value)
+}
+ catch(error){
+    display.value = "Error!"
+ }
+}
+
+
+function clearDisplay()
+{
+display.value = ""
+}
+
+function deleteValue()
+{
+    display.value = display.value.toString().slice(0,-1);
+}
